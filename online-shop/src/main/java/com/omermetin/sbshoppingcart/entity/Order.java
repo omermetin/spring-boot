ce397c18a17e2f43b9076f47536a0ef9ced5aac6
omer <omer@omer-desktop>
2019-04-22 21:06:13 +0100

    online-shop

--- online-shop/src/main/java/com/omermetin/sbshoppingcart/entity/Order.java
+++ online-shop/src/main/java/com/omermetin/sbshoppingcart/entity/Order.java
@@ -2,15 +2,11 @@ package com.omermetin.sbshoppingcart.entity;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
-import com.omermetin.sbshoppingcart.model.OrderDetailInfo;
  
 @Entity
 @Table(name = "Orders", //
